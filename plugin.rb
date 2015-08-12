4184e7271b303d8b493c97ecb8243fa5e8f30e6b
Sam <sam.saffron@gmail.com>
2015-08-12 14:35:44 +1000

    add code from omniauth salesforce

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,8 @@
 
 require 'auth/oauth2_authenticator'
 require 'omniauth-oauth2'
+require 'openssl'
+require 'base64'
 
 class SalesforceAuthenticator < ::Auth::OAuth2Authenticator
 
@@ -28,38 +30,86 @@ after_initialize do
   end
 end
 
+#
+# Class is mostly cut and paste from MIT https://raw.githubusercontent.com/realdoug/omniauth-salesforce/master/lib/omniauth/strategies/salesforce.rb
 class OmniAuth::Strategies::Salesforce < OmniAuth::Strategies::OAuth2
   # Give your strategy a name.
   option :name, "salesforce"
 
-  # This is where you pass the options you would pass when
-  # initializing your consumer from the OAuth gem.
-  #
-  #
+  MOBILE_USER_AGENTS =  'webos|ipod|iphone|ipad|android|blackberry|mobile'
 
-  # These are called after authentication has succeeded. If
-  # possible, you should try to set the UID without making
-  # additional calls (if the user id is returned with the token
-  # or as a URI parameter). This may not be possible with all
-  # providers.
-  uid { raw_info['id'].to_s }
+  option :client_options, {
+    :authorize_url => '/services/oauth2/authorize',
+    :token_url     => '/services/oauth2/token'
+  }
+  option :authorize_options, [
+    :scope,
+    :display,
+    :immediate,
+    :state,
+    :prompt,
+    :redirect_uri,
+    :login_hint
+  ]
+
+  def request_phase
+    req = Rack::Request.new(@env)
+    options.update(req.params)
+    ua = req.user_agent.to_s
+    if !options.has_key?(:display)
+      mobile_request = ua.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
+      options[:display] = mobile_request ? 'touch' : 'page'
+    end
+    super
+  end
+
+  def auth_hash
+    signed_value = access_token.params['id'] + access_token.params['issued_at']
+    raw_expected_signature = OpenSSL::HMAC.digest('sha256', options.client_secret.to_s, signed_value)
+    expected_signature = Base64.strict_encode64 raw_expected_signature
+    signature = access_token.params['signature']
+    fail! "Salesforce user id did not match signature!" unless signature == expected_signature
+    super
+  end
+
+  uid { raw_info['id'] }
 
   info do
     {
-      :name => raw_info['name'],
-      :email => raw_info['email']
+      'name'            => raw_info['display_name'],
+      'email'           => raw_info['email'],
+      'nickname'        => raw_info['nick_name'],
+      'first_name'      => raw_info['first_name'],
+      'last_name'       => raw_info['last_name'],
+      'location'        => '',
+      'description'     => '',
+      'image'           => raw_info['photos']['thumbnail'] + "?oauth_token=#{access_token.token}",
+      'phone'           => '',
+      'urls'            => raw_info['urls']
     }
   end
 
-  extra do
-    {
-      'raw_info' => raw_info
-    }
+  credentials do
+    hash = {'token' => access_token.token}
+    hash.merge!('instance_url' => access_token.params["instance_url"])
+    hash.merge!('refresh_token' => access_token.refresh_token) if access_token.refresh_token
+    hash
   end
 
   def raw_info
-    @raw_info ||= access_token.get('/oauth/me.json').parsed
+    access_token.options[:mode] = :header
+    @raw_info ||= access_token.post(access_token['id']).parsed
   end
+
+  extra do
+    raw_info.merge({
+      'instance_url' => access_token.params['instance_url'],
+      'pod' => access_token.params['instance_url'],
+      'signature' => access_token.params['signature'],
+      'issued_at' => access_token.params['issued_at']
+    })
+  end
+
 end
 
 auth_provider title: 'Sign in with Salesforce',
